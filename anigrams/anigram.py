56b36696cf373c03312bf89bad624b2f85f6a518
Matthew ODonnell <modonnell@covermymeds.com>
2019-08-13 13:11:40 -0400

    lunch poking

--- anigrams/anigram.py
+++ anigrams/anigram.py
@@ -1,5 +1,6 @@
 import functools
 import itertools
+import collections
 
 s = set(map(lambda w: w.replace("\n", ''), open("wordlist.txt", encoding='utf-8', errors='surrogateescape').readlines()))
 
@@ -9,11 +10,14 @@ def perms(word):
 def real_words(ps):
     return set(filter(lambda word: word in s, ps))
 
-
-#filter(lambda y: y  in s, set(map(lambda x: ''.join(x), itertools.permutations(list('clotted')))))
+def count_anitgram():
+    return map(lambda kv: kv[1], functools.reduce(lambda memo, x: [memo[tuple(sorted(dict(collections.Counter(list(x))).items()))].append(x), memo][1], s, collections.defaultdict(list)).items())
 
 def anigram():
     s2 = ['vang', 'BSDs',  'prizewoman',  'unessence', 'lenitives', 'pawnor', 'alible', 'chameleons', 'clotted', 'spiders', 'bazooms', 'wombats', 'Nampa', 'sperrylite', 'trawling', 'comically', 'labile']
     s3 = ['labile', 'vang']
     return functools.reduce(lambda memo, word: memo +  list(real_words(perms(word)) - set(memo)) + [None], s, list())
 
+
+def hash_anigram():
+    pass
