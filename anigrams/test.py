b5771dfdbc7c0c32362c533d071cddd68e08ed96
Matthew ODonnell <modonnell@covermymeds.com>
2019-08-22 13:29:02 -0400

    old anigram stuff

--- anigrams/test.py
+++ anigrams/test.py
@@ -6,11 +6,14 @@ import timeit
 
 class FooTest(unittest.TestCase):
 
-    def test_bar(self):
+    #def test_slow_alg(self):
         #print(list(perms('labile')))
         #print(list(perms('labile'))[0] in anigram.s)
         #print(list(real_words(perms('labile'))))
         #print("after")
-        print(timeit.Timer('import anigram; print(anigram.anigram())', 'gc.enable()').timeit(1))
-        
-        self.assertEqual(0,1)
+        #print(timeit.Timer('import anigram; print(anigram.anigram())', 'gc.enable()').timeit(1))
+
+
+    def test_counter_group(self):
+        print(anigram.count_anitgram())
+
